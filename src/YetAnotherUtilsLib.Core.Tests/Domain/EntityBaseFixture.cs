1d867d76162d302325bdc47ed5418dccbe04e812
Stefan Sedich <stefan.sedich@gmail.com>
2010-07-13 11:21:54 +1000

    Modified   src/YetAnotherUtilsLib.Core.Tests/Domain/EntityBaseFixture.cs

--- src/YetAnotherUtilsLib.Core.Tests/Domain/EntityBaseFixture.cs
+++ src/YetAnotherUtilsLib.Core.Tests/Domain/EntityBaseFixture.cs
@@ -12,8 +12,8 @@ namespace YetAnotherUtilsLib.Core.Tests.Domain
         {
             var id = Guid.NewGuid();
 
-            var a = new TestEnttiy() { Id = id };
-            var b = new TestEnttiy() { Id = id };
+            var a = new TestEntity() { Id = id };
+            var b = new TestEntity() { Id = id };
 
             Assert.IsTrue(a == b);
         }
@@ -23,8 +23,8 @@ namespace YetAnotherUtilsLib.Core.Tests.Domain
         {
             var id = Guid.NewGuid();
 
-            var a = new TestEnttiy();
-            var b = new TestEnttiy();
+            var a = new TestEntity();
+            var b = new TestEntity();
 
             Assert.IsFalse(a == b);
         }
@@ -32,8 +32,8 @@ namespace YetAnotherUtilsLib.Core.Tests.Domain
         [Test]
         public void Two_Entities_Of_Same_Type_With_Different_Ids_Are_Not_Equal()
         {
-            var a = new TestEnttiy() { Id = Guid.NewGuid() };
-            var b = new TestEnttiy() { Id = Guid.NewGuid() };
+            var a = new TestEntity() { Id = Guid.NewGuid() };
+            var b = new TestEntity() { Id = Guid.NewGuid() };
 
             Assert.IsFalse(a == b);
         }
@@ -43,8 +43,8 @@ namespace YetAnotherUtilsLib.Core.Tests.Domain
         {
             var id = Guid.NewGuid();
 
-            var a = new TestEnttiy() { Id = id };
-            var b = new TestEnttiy2() { Id = id };
+            var a = new TestEntity() { Id = id };
+            var b = new TestEntity2() { Id = id };
 
             Assert.IsFalse(a == b);
         }
@@ -54,8 +54,8 @@ namespace YetAnotherUtilsLib.Core.Tests.Domain
         {
             var id = Guid.NewGuid();
 
-            var a = new TestEnttiy();
-            var b = new TestEnttiy2();
+            var a = new TestEntity();
+            var b = new TestEntity2();
 
             Assert.IsFalse(a == b);
         }
@@ -63,7 +63,7 @@ namespace YetAnotherUtilsLib.Core.Tests.Domain
         [Test]
         public void New_Entity_Is_Transient()
         {
-            var modelA = new TestEnttiy();
+            var modelA = new TestEntity();
 
             Assert.IsTrue(modelA.IsTransient);
         }
@@ -71,7 +71,7 @@ namespace YetAnotherUtilsLib.Core.Tests.Domain
         [Test]
         public void Entity_Is_Not_Transient_When_Id_Is_Not_Empty()
         {
-            var modelA = new TestEnttiy() { Id = Guid.NewGuid() };
+            var modelA = new TestEntity() { Id = Guid.NewGuid() };
 
             Assert.IsFalse(modelA.IsTransient);
         }
@@ -80,7 +80,7 @@ namespace YetAnotherUtilsLib.Core.Tests.Domain
         public void GetHashCode_Returns_HashCode_Of_Id()
         {
             // Arrange
-            var model = new TestEnttiy {Id = Guid.NewGuid()};
+            var model = new TestEntity {Id = Guid.NewGuid()};
 
             // Act
             int hashCode = model.GetHashCode();
@@ -89,7 +89,7 @@ namespace YetAnotherUtilsLib.Core.Tests.Domain
             Assert.AreEqual(hashCode, model.Id.GetHashCode());
         }
 
-        internal class TestEnttiy : EntityBase<Guid> { }
-        internal class TestEnttiy2 : EntityBase<Guid> { }
+        internal class TestEntity : EntityBase<Guid> { }
+        internal class TestEntity2 : EntityBase<Guid> { }
     }
 }
