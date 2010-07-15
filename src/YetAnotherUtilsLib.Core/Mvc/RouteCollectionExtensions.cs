686f52394448269792a6e6ee0d55a29a8dcf009d
stefansedich <stefan.sedich@gmail.com>
2010-07-15 23:29:05 +1000

    * Adding LowercaseRoute support as per:
    http://stackoverflow.com/questions/878578/how-can-i-have-lowercase-routes-in-asp-net-mvc
    
    Modified   src/YetAnotherUtilsLib.Core/YetAnotherUtilsLib.Core.csproj
    Add        src/YetAnotherUtilsLib.Core/Mvc/LowercaseRoute.cs
    Add        src/YetAnotherUtilsLib.Core/Mvc/RouteCollectionExtensions.cs

---
+++ src/YetAnotherUtilsLib.Core/Mvc/RouteCollectionExtensions.cs
@@ -0,0 +1,29 @@
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace YetAnotherUtilsLib.Core.Mvc
+{
+    /// <summary>
+    /// http://stackoverflow.com/questions/878578/how-can-i-have-lowercase-routes-in-asp-net-mvc
+    /// </summary>
+    public static class RouteCollectionExtensions
+    {
+        public static Route MapRouteLowerCase(this RouteCollection routes, string name, string url, object defaults)
+        {
+            return routes.MapRouteLowerCase(name, url, defaults, null);
+        }
+
+        public static Route MapRouteLowerCase(this RouteCollection routes, string name, string url, object defaults, string constraints)
+        {
+            Route route = new LowercaseRoute(url, new MvcRouteHandler())
+            {
+                Defaults = new RouteValueDictionary(defaults),
+                Constraints = new RouteValueDictionary(constraints)
+            };
+
+            routes.Add(name, route);
+
+            return route;
+        }
+    }
+}
